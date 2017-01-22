fd83804ddcab8e2ca46f5024528307c151351708
Michal Dvorak <michal@mdvorak.org>
2017-01-22 14:37:47 +0100

    Renamed example app component

--- example/app/app.component.ts
+++ example/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ApiLocation } from 'angular-resource-router';
 
 @Component({
-    selector: 'example-app',
+    selector: 'my-app',
     template: `See <a href="https://app.swaggerhub.com/api/mdvorak/resource-router-example/1.0.0">API definition</a>
 
 <resource-outlet [(src)]="apiLocation.url"></resource-outlet>`
