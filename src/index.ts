584115a63ef92d9bf6e0e10a214b77f59b32f401
Michal Dvorak <michal@mdvorak.org>
2017-03-21 21:47:58 +0100

    Added missing exports to index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-export { ResourceRouterModule, ResourceRouterOptions } from './resource-router-module';
+export { ResourceRouterModule, ResourceRouterOptions, RESOURCE_ROUTER_CONFIGURATION } from './resource-router-module';
 export { ViewData } from './view-data';
 export { ViewDataLoader, HttpViewDataLoader, DefaultHttpViewDataLoader } from './view-data-loader';
-export { ViewDef, Data } from './view-definition';
+export { ViewDef, Data, StatusType } from './view-definition';
 export { ApiUrl, APP_API_PREFIX } from './api-url';
 export { ApiLocation } from './api-location';
-export { ResourceViewRegistry, RESOURCE_VIEWS } from './resource-view-registry'
+export { ResourceViewRegistry, RESOURCE_VIEWS, TYPE_QUALITY_EVALUATOR } from './resource-view-registry'
 export { ResourceOutletComponent } from './directives/resource-outlet';
 export { ApiLinkDirective } from './directives/api-link.directive';
 export { ResourceDataDirective } from './directives/resource-data';
 export { ResourceViewDirective } from './directives/resource-view';
+export { TypeQualityEvaluator, simpleTypeQualityEvaluator, statusQualityEvaluator } from './quality-evaluator';
+export { NavigationHandler } from './navigation-handler'
+export { normalizeUrl, normalizeMediaType }from './normalize';
+export * from './system-media-types';
+export { ViewTypeStrategy, ContentTypeStrategy } from './view-type-strategy';
+export { escapeRegExpPattern, wildcardToRegex } from './utils/wildcard-to-regex';
--- src/navigation-handler.ts
+++ src/navigation-handler.ts
@@ -1,19 +1,7 @@
+// TODO null?
 export type UrlType = string|null;
 
 export interface NavigationHandler {
   url: UrlType;
   go(url: string): void;
 }
-
-export const NULL_NAVIGATION_HANDLER = {
-
-  set url(url: UrlType) {
-  },
-
-  get url(): UrlType {
-    return null;
-  },
-
-  go(url: string): void {
-  }
-};
