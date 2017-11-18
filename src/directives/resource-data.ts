05d02719550745e01db5ac02434c6b689f19dc0f
Michal Dvorak <michal@mdvorak.org>
2017-11-18 18:32:55 +0100

    resourceData now propagates back the actual url of the response

--- src/directives/resource-data.ts
+++ src/directives/resource-data.ts
@@ -3,9 +3,10 @@ import 'rxjs/add/operator/switchMap';
 import { Observable } from 'rxjs/Observable';
 import { ViewDataLoader } from '../view-data-loader';
 import { NO_HEADERS, ViewData } from '../view-data';
-import { NavigationHandler, UrlType } from '../navigation-handler';
+import { NavigationHandler } from '../navigation-handler';
 import { ResourceViewRegistry } from '../resource-view-registry';
 import { MEDIA_TYPE_ROUTER_EMPTY, MEDIA_TYPE_ROUTER_ERROR, MEDIA_TYPE_ROUTER_LOADING } from '../system-media-types';
+import { Subject } from 'rxjs/Subject';
 
 
 export class ResourceDataContext {
@@ -19,9 +20,11 @@ export class ResourceDataContext {
 })
 export class ResourceDataDirective implements OnInit, NavigationHandler {
 
-  @Output() urlChange = new EventEmitter<UrlType>();
+  @Output()
+  readonly urlChange = new EventEmitter<string>();
 
-  private urlValue: UrlType;
+  private urlValue = '';
+  private urlSubject = new Subject<string>();
   private context: ResourceDataContext;
 
   constructor(protected viewContainer: ViewContainerRef,
@@ -30,13 +33,23 @@ export class ResourceDataDirective implements OnInit, NavigationHandler {
               protected registry: ResourceViewRegistry) {
     this.context = new ResourceDataContext(this.mockView('', MEDIA_TYPE_ROUTER_LOADING, 204, 'OK'));
 
-    // Handle src changes
-    this.urlChange
+    // Handle url changes
+    this.urlSubject
       .switchMap(url => this.load(url))
-      .subscribe(data => this.context.$implicit = data);
+      .subscribe(data => {
+        // Update data
+        this.context.$implicit = data;
+
+        // This propagates back the actual url when server-side redirect was performed
+        if (data.url !== this.urlValue) {
+          this.urlValue = data.url;
+          this.urlChange.emit(this.urlValue);
+        }
+      });
   }
 
   // Unused but needed when used in decomposed notation directly on <template>
+  // noinspection JSUnusedLocalSymbols
   @Input()
   set resourceData(value: any) {
   }
@@ -46,18 +59,18 @@ export class ResourceDataDirective implements OnInit, NavigationHandler {
     this.url = value;
   }
 
-  get url(): UrlType {
+  get url(): string {
     return this.urlValue;
   }
 
-  set url(value: UrlType) {
+  set url(value: string) {
     if (this.urlValue !== value) {
       this.urlValue = value;
-      this.urlChange.emit(value);
+      this.urlSubject.next(value);
     }
   }
 
-  load(url: UrlType): Observable<ViewData<any>> {
+  load(url: string): Observable<ViewData<any>> {
     if (url) {
       return this.loader
         .fetch(url, this)
@@ -72,9 +85,14 @@ export class ResourceDataDirective implements OnInit, NavigationHandler {
   }
 
   go(url: string): void {
-    // Always fire the event, which forces the data to be reloaded
-    this.urlValue = url;
-    this.urlChange.emit(url);
+    // Update property if changed
+    if (this.urlValue !== url) {
+      this.urlValue = url;
+      this.urlChange.emit(url);
+    }
+
+    // Load data (always)
+    this.urlSubject.next(this.urlValue);
   }
 
   private mockView(url: string, type: string, status: number, statusText: string, body?: any): ViewData<any> {
