0f2d8f1fc79561a18be29fc9d181806731d0c658
Michal Dvorak <michal@mdvorak.org>
2017-12-31 17:27:12 +0100

    Added snapshot to ActivatedView (relates to #9)

--- src/lib/src/activated-view.ts
+++ src/lib/src/activated-view.ts
@@ -1,10 +1,18 @@
 import { Navigable } from './navigable';
 import { Observable } from 'rxjs/Observable';
 import { ViewData } from './view-data';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 export class ActivatedView<T> {
 
+  public readonly data: Observable<ViewData<T>>;
+
   constructor(public readonly navigation: Navigable,
-              public readonly data: Observable<ViewData<T>>) {
+              private readonly _data: BehaviorSubject<ViewData<T>>) {
+    this.data = _data;
+  }
+
+  get snapshot() {
+    return this._data.getValue();
   }
 }
