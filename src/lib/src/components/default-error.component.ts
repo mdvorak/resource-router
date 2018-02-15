89689026217d3e57291e7dd0ad3bb473781e8125
Michal Dvorak (cen38289) <michadvorak@csas.cz>
2018-02-15 13:26:12 +0100

    Improved DefaultErrorComponent to render HTML when received
    This has a con, when <head> contains something, it is rendered as text,
    even style or script tags.
    Added docs to DefaultErrorComponent

--- src/lib/src/components/default-error.component.ts
+++ src/lib/src/components/default-error.component.ts
@@ -2,20 +2,53 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedView } from '../activated-view';
 import { ViewData } from '../view-data';
 
+
+/**
+ * Default view that is used when no other view has been matched.
+ *
+ * To replace it, register own view with `*` type nad status (don't forget `responseType`):
+ * ```ts
+ * ResourceRouterModule.forTypes([
+ *   {
+ *     component: MyGenericErrorComponent,
+ *     status: '*',
+ *     type: '*',
+ *     responseType: 'text'
+ *   }
+ * ]);
+ * ```
+ *
+ * If you want to have custom view only for specific content type (e.g. specific defined error), register it as follows:
+ * ```ts
+ * ResourceRouterModule.forTypes([
+ *   {
+ *     component: MyApiErrorComponent,
+ *     status: '*',
+ *     type: 'application/vnd.company.error',
+ *     responseType: 'json'
+ *   }
+ * ]);
+ * ```
+ * Note: since `responseType: 'json'` is default, it can be omitted.
+ *
+ * Both above views can co-exist - when well-defined error is received, `MyApiErrorComponent` is used,
+ * and for unexpected there is `MyGenericErrorComponent`.
+ */
 @Component({
-  template: `<p>Error loading a view<code *ngIf="data.type"> {{data.type}}</code> from resource
-    <code>{{data.url}}</code>
-  <p>{{data.status}} {{data.statusText}}</p>
-  <pre *ngIf="data.body">{{data.body}}</pre>`
+  templateUrl: './default-error.component.html'
 })
 export class DefaultErrorComponent implements OnInit {
 
-  public data: ViewData<any>;
+  data: ViewData<any>;
 
   constructor(public view: ActivatedView<any>) {
   }
 
   ngOnInit(): void {
     this.view.data.subscribe(data => this.data = data);
+  }
+
+  get html() {
+    return this.data.type === 'text/html';
   }
 }
