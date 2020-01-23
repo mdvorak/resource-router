4eed8c38e45f648e487d814d42ff1e249ee79c50
David Kolar (ext90796) <david.kolar@ext.csas.cz>
2020-01-23 21:01:03 +0100

    directive resourceLinkActive

---
+++ src/lib/src/directives/resource-link-active.ts
@@ -0,0 +1,44 @@
+import { Directive, Input, ElementRef, Renderer2, AfterContentInit, OnDestroy } from '@angular/core';
+import { ApiLocation } from '../api-location';
+import { Subscription } from 'rxjs';
+
+@Directive({
+    selector: '[resourceLinkActive]'
+})
+export class ResourceLinkActiveDirective implements AfterContentInit, OnDestroy {
+
+    @Input() resourceLink: string;
+
+    private subscription: Subscription;
+    private classes: string[] = [];
+
+    constructor (
+        private render: Renderer2,
+        private el: ElementRef,
+        private apiLocation: ApiLocation
+    ) {
+        this.subscription = apiLocation.urlChange.subscribe(() => this.update());
+    }
+
+    @Input() set resourceLinkActive(data: string[]|string) {
+        const classes = Array.isArray(data) ? data : data.split(' ');
+        this.classes = classes.filter(c => !!c);
+    }
+
+    ngAfterContentInit(): void { this.update(); }
+
+    ngOnDestroy(): void { this.subscription.unsubscribe(); }
+
+    private update(): void {
+        const url = this.apiLocation.url;
+        const isActive = url.includes(this.resourceLink);
+
+        this.classes.forEach((_class: string) => {
+            if (isActive) {
+               this.render.addClass(this.el.nativeElement, _class);
+            } else {
+                this.render.removeClass(this.el.nativeElement, _class);
+            }
+        });
+    }
+}
