455ce5ea903c9381b0fd452c02bbe37376579b91
David Kolar (ext90796) <david.kolar@ext.csas.cz>
2020-02-04 22:39:34 +0100

    Revert "uprava parametru pro testy"
    This reverts commit 30d0ba30e5a167ad3caa9ad42840fc961cfb9898.

--- karma.conf.js
+++ karma.conf.js
@@ -32,14 +32,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     singleRun: false,
-    browserDisconnectTimeout: 10000,
-    browserDisconnectTolerance: 3,
-    browserNoActivityTimeout: 60000,
-    flags: [
-      '--disable-web-security',
-      '--disable-gpu',
-      '--no-sandbox'
-    ],
     detectBrowsers: {
       postDetection: function (browsers) {
         // When overriden, use command line
