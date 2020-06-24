3c4b0bce0be1eb150391f9f6fae036ec1de2d6cc
Michal Dvorak <michal@mdvorak.org>
2020-06-24 10:48:47 +0200

    updated dependencies, removed karma-detect-browsers
    hardcoded browsers Chrome, Firefox and IE
    minor updates to config files according to Angular 9 standards

--- karma.conf.js
+++ karma.conf.js
@@ -4,15 +4,12 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['detectBrowsers', 'jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-edge-launcher'),
       require('karma-firefox-launcher'),
       require('karma-ie-launcher'),
-      require('karma-chrome-launcher'),
-      require('karma-detect-browsers'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -21,38 +18,17 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: ['html', 'lcovonly'],
+      dir: require('path').join(__dirname, './coverage/angular-resource-router'),
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+    browsers: ['ChromeHeadless', 'FirefoxHeadless', 'IE'],
     singleRun: false,
-    detectBrowsers: {
-      postDetection: function (browsers) {
-        // When overriden, use command line
-        if (config.browsers && config.browsers.length) {
-          console.log('Using browsers from command-line argument', config.browsers);
-          return config.browsers;
-        }
-
-        // Run in headless mode
-        browsers = browsers.filter(function (b) {
-          return ['PhantomJS', 'Edge'].indexOf(b) < 0;
-        }).map(function (b) {
-          if (b === 'Chrome' || b === 'Firefox') {
-            return b + 'Headless';
-          }
-          return b;
-        });
-
-        // Log and return
-        console.log('Using filtered detected browsers', browsers);
-        return browsers;
-      }
-    }
+    restartOnFileChange: true
   });
 };
